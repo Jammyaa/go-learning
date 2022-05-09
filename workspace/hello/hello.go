cb59c9f503f94f373ec8b64603486f220fb5b8c8
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-09 21:04:23 +0700

    create some function in workspace

--- workspace/hello/hello.go
+++ workspace/hello/hello.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-
+	"example.com/math"
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
 	fmt.Println(stringutil.Reverse("Hello"))
 	fmt.Println(stringutil.ToUpper("Hello"))
+	fmt.Println(math.Add(3,4))
+	fmt.Println(math.Sub(3,4))
 }
