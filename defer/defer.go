01f832cde1accbca866d3573b38d2374d8193782
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-10 20:22:30 +0700

    add explaining about defer statement

--- defer/defer.go
+++ defer/defer.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Trì hoãn (defer) là một khái niệm khá mới trong điều khiển luồng. Nó cho phép một câu lệnh được gọi ra nhưng không thực thi ngay mà hoãn lại đến khi các lệnh xung quanh trả về kết quả.
 func main() {
 	fmt.Println("counting")
 
