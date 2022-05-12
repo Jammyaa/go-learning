c3c31303a159197b0ae54f6766361d00102a1661
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-13 02:49:17 +0700

    learn about slices literal

--- slices/slices.go
+++ slices/slices.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	// names is a array of strings
 	names := [4]string{
 		"Chinh",
 		"Linh",
@@ -12,8 +14,8 @@ func main() {
 
 	fmt.Println(names)
 
-	a := names[0:2]
-	b := names[1:3]
+	a := names[0:2] // This is a slice
+	b := names[1:3] // This is a slice
 
 	fmt.Println(a, b)
 
@@ -21,4 +23,23 @@ func main() {
 
 	fmt.Println(a, b)
 	fmt.Println(names)
+
+	// A slice literal is like an array literal without the length.
+	q := []int{2, 4, 5, 6, 7, 8, 9}
+	fmt.Println(q)
+	r := []bool{true, false, true, false, true, false, false}
+	fmt.Println(r)
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{4, false},
+		{5, true},
+		{6, false},
+		{7, true},
+		{8, false},
+		{9, false},
+	}
+	fmt.Println(s)
 }
