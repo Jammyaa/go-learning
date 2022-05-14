d0d4845f44f5a6cdb5513342d35dc35873822498
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-14 13:32:23 +0700

    map literals continued

--- map/map.go
+++ map/map.go
@@ -38,4 +38,12 @@ func main() {
 	}
 
 	fmt.Println(m2)
+
+	// map literals continued
+	// If the top-level type is just a type name, you can omit it from the elements of the literal.
+	var m3 = map[string]Vertex{
+		"Bell Labs": {40.68433, -74.39967},
+		"Google":    {37.42202, -122.08408},
+	}
+	fmt.Println(m3)
 }
