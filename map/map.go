6f552646e435761e33d9d98f4f905804052df3bf
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-14 11:44:45 +0700

    learn about map in go

---
+++ map/map.go
@@ -0,0 +1,23 @@
+// A map maps keys to values.
+
+// The zero value of a map is nil. A nil map has no keys, nor can keys be added.
+
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+	// The make function returns a map of the given type, initialized and ready for use.
+	m = make(map[string]Vertex)
+	m["Notional Labs"] = Vertex{
+		100, 200,
+	}
+	fmt.Println(m)
+	fmt.Println(m["Notional Labs"])
+}
