e90debbf58f88e9589b9c8b7ae22dfd765c314d0
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-14 10:31:26 +0700

    skip the first or second return value of each range iteration

--- range/range.go
+++ range/range.go
@@ -4,7 +4,21 @@ import "fmt"
 
 func main() {
 	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128, 25, 512, 1024}
+
+	// When ranging over a slice, two values are returned for each iteration. The first "i" is the index, and the second "v" is a copy of the element at that index.
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+	pow2 := make([]int, 11)
+
+	// If you only want the index, you can omit the second variable.
+	for i := range pow2 {
+		pow2[i] = 1 << uint(i)
+	}
+
+	// You can skip the index or value by assigning to _
+	for _, v := range pow2 {
+		fmt.Printf("%d\n", v)
 	}
 }
