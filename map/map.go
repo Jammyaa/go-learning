5dc7d0bce56028a04254e1505d59a3dc1dd382a5
chinhnotional <eyescryptoinsights@gmail.com>
2022-05-14 13:28:18 +0700

    learn about map in go

--- map/map.go
+++ map/map.go
@@ -10,13 +10,16 @@ type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
+var m map[string]Vertex // Keys are string, value are Vertex
 
 func main() {
 	// The make function returns a map of the given type, initialized and ready for use.
 	m = make(map[string]Vertex)
 	m["Notional Labs"] = Vertex{
 		100, 200,
+	}
+	m["Chinh"] = Vertex{
+		200, 300,
 	}
 	fmt.Println(m)
 	fmt.Println(m["Notional Labs"])
